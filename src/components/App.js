d5e0bdbe758c7bd483e6d9913742f5920da57210
hossein <h.beig@yahoo.com>
2018-12-16 20:49:38 +0800

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SongList from './SongList';
+import SongDetail from './SongDetail';
 
 const App = () => {
   return (
@@ -12,15 +13,16 @@ const App = () => {
         <div className="short line" />
         <div className="very short line" />
       </div>
+      <div>
+        <h2 className="ui container header">
+          <i className="headphones icon" />
+          <div className="content">Pick Your Song</div>
 
-      <h2 className="ui container header">
-        <i className="headphones icon" />
-        <div className="content">Pick Your Song</div>
-
-        <a className="ui orange tag label">
-          Persian <i style={{ paddingLeft: '10px' }} i className="ir flag" />
-        </a>
-      </h2>
+          <a className="ui orange tag label">
+            Persian <i style={{ paddingLeft: '10px' }} i className="ir flag" />
+          </a>
+        </h2>
+      </div>
       <div className="ui container placeholder">
         <div className="medium line" />
       </div>
@@ -29,6 +31,9 @@ const App = () => {
         <div className="ui row">
           <div className="column eight wide">
             <SongList />
+          </div>
+          <div className="column eight wide">
+            <SongDetail />
           </div>
         </div>
       </div>
---
+++ src/components/SongDetail.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return <h3>Select a song</h3>;
+  }
+  return (
+    <div className="ui placeholder">
+      <h3>Details for:</h3>
+      <p>
+        Title: {song.title} <br />
+        Duration: {song.duration}
+      </p>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return { song: state.selectedSong };
+};
+export default connect(mapStateToProps)(SongDetail);
